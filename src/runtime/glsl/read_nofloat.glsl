3b4a3a02c78594a3680bb187e6f9672ac3c37110
Kevin Kwok <antimatter15@gmail.com>
2017-01-07 01:25:44 -0500

    fixing readTensorChannel

--- src/runtime/glsl/read_nofloat.glsl
+++ src/runtime/glsl/read_nofloat.glsl
@@ -1,13 +1,15 @@
 // READ_TENSOR_NOFLOAT
 
-vec4 readTensorChannel(Tensor t, ivec4 pos, int ch){
-    float value = decode_float(texture2D(t.tex, (
+float readTensorChannel(Tensor t, ivec4 pos, int ch){
+    return decode_float(texture2D(t.tex, (
         vec2(tile2vec(
             vec2tile(pos.zw, ceildiv(t.shape.z, 4))
         , t.cols) * t.shape.xy) +
         vec2(pos.x * 4 + ch, pos.y) + vec2(0.5, 0.5)
     ) / vec2(t.texSize)));
+}
 
+vec4 _readTensorChannel(Tensor t, ivec4 pos, int ch){
     if(ch == 0){
         return vec4(value, 0, 0, 0);
     }else if(ch == 1){
@@ -20,13 +22,13 @@ vec4 readTensorChannel(Tensor t, ivec4 pos, int ch){
 }
 
 vec4 readTensor(Tensor t, ivec4 pos){
-    return readTensorChannel(t, pos, 0) +
-         + readTensorChannel(t, pos, 1)
-         + readTensorChannel(t, pos, 2)
-         + readTensorChannel(t, pos, 3);
+    return _readTensorChannel(t, pos, 0) +
+         + _readTensorChannel(t, pos, 1)
+         + _readTensorChannel(t, pos, 2)
+         + _readTensorChannel(t, pos, 3);
 }
 
 vec4 readTensorX(Tensor t, ivec4 pos){
     int ch = imod(int(gl_FragCoord.x), 4);
-    return readTensorChannel(t, pos, ch);
+    return _readTensorChannel(t, pos, ch);
 }
--- src/runtime/glsl/read_normal.glsl
+++ src/runtime/glsl/read_normal.glsl
@@ -13,7 +13,7 @@ vec4 readTensorX(Tensor t, ivec4 pos){
     return readTensor(t, pos);
 }
 
-vec4 readTensorChannel(Tensor t, ivec4 pos, int ch){
+float readTensorChannel(Tensor t, ivec4 pos, int ch){
 	if(ch == 0) return readTensor(t, pos).r;
 	if(ch == 1) return readTensor(t, pos).g;
 	if(ch == 2) return readTensor(t, pos).b;
