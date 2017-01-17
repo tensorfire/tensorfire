b7dca5d4dd71cb2106b4acfc91d70a3f54491caa
Kevin Kwok <antimatter15@gmail.com>
2017-01-17 01:28:58 -0500

    simpler

--- src/format/tile-nofloat/read.glsl
+++ src/format/tile-nofloat/read.glsl
@@ -4,11 +4,11 @@
 float decode_float(vec4 val){
     vec4 scl = floor(255.0 * val + 0.5);
     float sgn = (scl.a < 128.0) ? 1.0 : -1.0;
-    float exn = float(imod(int(scl.a) * 2, 256)) + floor(scl.b / 128.0) - 127.0;
+    float exn = mod(scl.a * 2.0, 256.0) + floor(scl.b / 128.0) - 127.0;
     float man = 1.0 +
         (scl.r / 8388608.0) + 
         (scl.g / 32768.0) +
-        float(imod(int(scl.b), 128)) / 128.0;
+        mod(scl.b, 128.0) / 128.0;
     return sgn * man * pow(2.0, exn);
 }
 
