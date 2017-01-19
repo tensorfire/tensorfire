bfddb4cf313409af5a2e3d91d44069c0a96246bf
Kevin Kwok <antimatter15@gmail.com>
2017-01-19 11:05:55 -0500

    1-4/pack/stride

---
+++ src/format/1-4/pack/stride/write.glsl
@@ -0,0 +1,20 @@
+uniform ivec2 @texSize;
+uniform ivec4 @shape;
+
+vec4 clampify(vec4 v){
+    return vec4(ivec4(clamp(v, vec4(0), vec4(1)) * 255.0)) / 255.0;
+}
+
+float processf(ivec4 pos);
+void main(){
+	int tile = vec2tile(ivec2(gl_FragCoord.xy), @texSize.x);
+	int chunks = @shape.x * @shape.y * @shape.z * @shape.w;
+	if(tile >= chunks){ checkerboard(); return; }
+
+	gl_FragColor = @encode14(@activation44(processf(ivec4(
+		imod(tile, @shape.x),
+		imod(tile / @shape.x, @shape.y),
+		imod(tile / @shape.x / @shape.y, @shape.z ),
+		tile / @shape.x / @shape.y / @shape.z
+	))));
+}
