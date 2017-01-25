1901530985a5108b369cfadc93708f9bfa7ea39a
Kevin Kwok <antimatter15@gmail.com>
2017-01-24 23:14:31 -0500

    removing stuff

--- src/format2-old/codec/softfloat/index.js
+++
@@ -1,4 +0,0 @@
-import { readFileSync } from 'fs';
-
-export const readShader = readFileSync(__dirname + '/read.glsl', 'utf8');
-export const writeShader = readFileSync(__dirname + '/write.glsl', 'utf8');
